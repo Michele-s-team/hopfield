6a1cdeb0892c8cf7308070e11addf1f9622ca5d9
dumontbast <dumontbast@gmail.com>
2026-04-08 12:26:40 +0200

    .

--- codes/main.cpp
+++ codes/main.cpp
@@ -13,11 +13,19 @@
 #include <getopt.h>
 //this has to be uncommented to run on mesopsl
 #include <stdint.h>
+#include <chrono>
+
 
 #include "gsl_rng.h"
 #include "gsl_math.h"
 #include "gsl_sf_log.h"
 #include "gsl_randist.h"
+
+#include "main.hpp"
+#include "int.hpp"
+#include "system_bits.hpp"
+#include "system_nobits.hpp"
+
 
 /*
  compile on mac
@@ -45,13 +53,6 @@
  o is the path where to store the results
  */
 
-#include "main.hpp"
-#include "system_bits.hpp"
-#include "system_nobits.hpp"
-
-//all entries of BitSet_one are equal to 1
-
-
 /*
  void UnsignedInt::SetRandom(unsigned long long int seed){
  
@@ -70,143 +71,145 @@
  }
  */
 
-//fill all the entries of BitSet_one and of Bits_one with 1, same for 0 with Bits_zero
+/*
+ BitSet BitSet_one;
+Bits Bits_one, Bits_zero;
+
+// Constants
+const int MAX_VALUE = 127;   // maximum random value
+const int N_SETS = 6;        // number of sets of integers
+
+int main() {
+    gsl_rng* ran = gsl_rng_alloc(gsl_rng_gfsr4);
+    gsl_rng_set(ran, 12345);
+
+    for (int game = 0; game < N_SETS; game++) {
+
+        vector<unsigned long long> A(n_bits);
+
+        // Generate random integers
+    for (int s = 0; s < n_bits; s++) {
+        A[s] = gsl_rng_uniform_int(ran, MAX_VALUE + 1);
+    }
+        UnsignedInt A_bit(MAX_VALUE), B_bit(MAX_VALUE);
+        A_bit.SetFromVector(&A);
+        string print = "A in the BitSet format";
+        A_bit.PrintBase10(cout);
+
+        // Reconstruct vector from Int
+        vector<unsigned long long> C(n_bits); // resize before filling
+        A_bit.GetBase10(C);
+
+        // Print results and compare
+        std::cout << "\n===== Game " << game << " =====\n";
+        for (int s = 0; s < n_bits; s++) {
+            cout << "s=" << s
+                      << " | Input =" << A[s]
+                      << " | Output =" << C[s];
+            if (A[s] != C[s]) {
+                cout << "  <-- ERROR";
+            }
+            else{cout << "   OK"; }
+            cout << std::endl;
+        }
+    }
+
+
+    return 0;
+}
+*/
+
+
+
+//all entries of BitSet_one are equal to 1
 BitSet BitSet_one;
 Bits Bits_one, Bits_zero;
 
 
-int main(int argc, char * argv[]) {
-    
-//    int options;
-//    unsigned long long int N=0, T=0, S=0, s, t;
-//    unsigned int i, seed=0;
-//    //the mean of the gain of the btiwise approach vs. the non-bitwise approach, and similarly for the mean of the square of the gain
-//    double gain_av, gain_pow_2_av;
-//    string output_directory;
-//    clock_t start_nobits, start_bits, end_nobits, end_bits;
-    
-//    cout.precision(cout_precision);
-//    BitSet_one.ResizeAndSetAll(1);
-//    for(i = 0; i<n_bits; i++){
-//        Bits_one.Set(i, true);
-//        Bits_zero.Set(i, false);
-//    }
-    
-    
-//    while ((options = getopt(argc, argv, ":N:T:S:s:o:")) != -1) {
-//        
-//        switch (options) {
-//                
-//            case 'N':
-//                N = ((unsigned int)atoi(optarg));
-//                break;  
-//                
-//            case 'T':
-//                T = ((unsigned int)gsl_pow_int(10, atoi(optarg)));
-//                break;
-//                
-//            case 'S':
-//                S = ((unsigned int)gsl_pow_int(10, atoi(optarg)));
-//                break;
-//                
-//            case 's':
-//                seed = ((unsigned int)atoi(optarg));
-//                break;
-//                
-//            case 'o':
-//                output_directory.assign(optarg);
-//                break;
-//                
-//                
-//        }
-//        
-//    }
-  
-    UnsignedInt I(24);
-    I.SetRandom((unsigned int)0);
-    I.PrintBase10(cout);
-    I.MultiplyByTwoTo();
-    I.PrintBase10(cout);
-    
-    
-    
-    //    TestUnsignedIntMultiply(S, seed);
-    //    SpeedTestUnsignedIntMultiply(128, S, seed);
-    //    TestUnsignedIntAddTo(S, seed);
-    //    TestDoubleAddTo(S, seed);
-    //    SpeedTestDoubleAddTo(S, seed);
-    //    SpeedTestUnsignedIntAddto(128, S, seed);
-    //        TestFractionFloorMultiply(S, seed);
-    //        SpeedTestFractionFloorMultiply(32, 128, S, seed);
-    
-    
-    
-    /*
-    for(gain_av=0.0, gain_pow_2_av=0.0, t=0; t<T; t++){
-        
-        cout << "* t = " << t;
-         
-        SystemBits frank_bits(N, (unsigned int)(seed+t), output_directory);
-        SystemNoBits frank_nobits(N, (unsigned int)(seed+t), output_directory);
-        
-        
-        // speed test without bits  *********************
-        start_nobits = clock();
-        for(s=0; s<n_bits*S; ++s){
-            frank_nobits.iterate();
+const int N_sets = 50000;
+const long long MAX_VALUE = 100000;
+
+int main() {
+
+    gsl_rng* ran = gsl_rng_alloc(gsl_rng_gfsr4);
+    gsl_rng_set(ran, 12345);
+
+    // =========================================
+    // INITIALISATION (NON MESURÉE)
+    // =========================================
+
+    // --- Bitsets ---
+    vector<UnsignedInt> A_bit;
+    vector<UnsignedInt> B_bit;
+
+    A_bit.reserve(N_sets);
+    B_bit.reserve(N_sets);
+
+    for (int i = 0; i < N_sets; i++) {
+
+        vector<unsigned long long> A(n_bits), B(n_bits);
+
+        for (int s = 0; s < n_bits; s++) {
+            A[s] = gsl_rng_uniform_int(ran, MAX_VALUE + 1);
+            B[s] = gsl_rng_uniform_int(ran, MAX_VALUE + 1);
         }
-        end_nobits = clock();
-        frank_nobits.outfile.close();
-        cout << endl << "\tTime for [n_bits*S] Iterate()s without bits = \t\t " << std::scientific << ((double)(end_nobits - start_nobits))/CLOCKS_PER_SEC << "s" <<  endl;
-        
-        
-        
-        // speed test with bits  *********************
-        start_bits = clock();
-        for(s=0; s<S; ++s){
-            frank_bits.Iterate();
-        }
-        end_bits = clock();
-        frank_bits.outfile.close();
-        cout << "\tTime for S Iterate()s with bits = \t\t         " << std::scientific << ((double)(end_bits - start_bits))/CLOCKS_PER_SEC << "s" <<  endl << endl;
-        
-        
-        gain_av += ((double)(end_nobits - start_nobits)) / ((double)(end_bits - start_bits));
-        gain_pow_2_av += gsl_pow_2(((double)(end_nobits - start_nobits)) / ((double)(end_bits - start_bits)));
-        
-        cout << "\tGain bits/nobits = " << ((double)(end_nobits - start_nobits)) / ((double)(end_bits - start_bits)) << endl;
-        
-        cout << endl << "\tdummy print";
-        frank_bits.L.PrintBase10(cout);
-        cout << "\t" << frank_bits.tau << frank_nobits.tau << frank_nobits.z << frank_nobits.rhs << endl;
-        //
-        
+
+        UnsignedInt A_tmp(MAX_VALUE), B_tmp(MAX_VALUE);
+        A_tmp.SetFromVector(&A);
+        B_tmp.SetFromVector(&B);
+
+        A_bit.push_back(A_tmp);
+        B_bit.push_back(B_tmp);
     }
-    
-    gain_av /= ((double)T);
-    gain_pow_2_av /= ((double)T);
-    cout << "\nlog_2(N),log_10(T),log_10(S),<Gain bits/nobits>,err_<Gain bits/nobits>:\n"
-    << log(N)/log(2) << ","
-    << log(T)/log(10.0) << ","
-    << log(S)/log(10.0)  << ","
-    << gain_av << ","
-    << sqrt(gain_pow_2_av - gsl_pow_2(gain_av))
-    << endl;
-    
-    cout << endl;
+
+    // --- Entiers scalaires ---
+    vector<long long> A_ref(N_sets * n_bits);
+    vector<long long> B_ref(N_sets * n_bits);
+
+    for (int i = 0; i < N_sets * n_bits; i++) {
+        A_ref[i] = gsl_rng_uniform_int(ran, MAX_VALUE + 1);
+        B_ref[i] = gsl_rng_uniform_int(ran, MAX_VALUE + 1);
+    }
+
+    // =========================================
+    // BENCHMARK BITSET
+    // =========================================
+
+    double clock_bitset = 0.0;
+
+    auto start_bit = chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < N_sets; i++) {
+        A_bit[i] += &B_bit[i];
+    }
+
+    auto end_bit = chrono::high_resolution_clock::now();
+    chrono::duration<double> dt_bit = end_bit - start_bit;
+    clock_bitset = dt_bit.count();
+
+    // =========================================
+    // BENCHMARK SCALAIRE
+    // =========================================
+
+    double clock_ref = 0.0;
+
+    auto start_ref = chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < N_sets * n_bits; i++) {
+        A_ref[i] += B_ref[i];
+    }
+
+    auto end_ref = chrono::high_resolution_clock::now();
+    chrono::duration<double> dt_ref = end_ref - start_ref;
+    clock_ref = dt_ref.count();
+
+    // =========================================
+    // RESULTATS
+    // =========================================
+
+    cout << "\nTotal clock_bitset: " << clock_bitset << " s\n";
+    cout << "Total clock_ref:    " << clock_ref << " s\n";
+
+    gsl_rng_free(ran);
     return 0;
-    */
-     
-    /*
-     double x = gsl_rng_uniform(Frank.ran);
-     Fraction R;
-     vector<bool> v(n_bits_mantissa);
-     R.Resize(n_bits_mantissa);
-     R.Fraction::SetAllFromDoubleMantissa(x, &v);
-     
-     
-     cout << "x:" << x << endl;
-     R.Fraction::PrintBase10("R");
-     */
-    
 }
--- codes/src/bitset.cpp
+++ codes/src/bitset.cpp
@@ -61,7 +61,7 @@ void BitSet::Resize(unsigned long long int size){
 
 
 //inline
-unsigned int BitSet::GetSize(void){
+unsigned int BitSet::GetSize(void) const{
     
     return ((unsigned int)(b.size()));
     
@@ -111,6 +111,25 @@ void BitSet::SetAll(unsigned long long int i){
         (b[s]).SetAll(false);
     }
     
+}
+
+
+void BitSet::SetFromVector(const vector<unsigned long long>* vec) {
+    if (!vec) return; // safety check
+
+    size_t n_cols = vec->size();
+
+    if (GetSize() < n_bits) Resize(n_bits);
+
+    for (size_t p = 0; p < n_bits; p++) {
+        for (size_t s = 0; s < n_cols; s++) {
+            b[p].Set(s, ((*vec)[s] >> p) & 1ULL);
+        }
+    }
+
+    for (size_t p = n_bits; p < GetSize(); p++) {
+        b[p].SetAll(false);
+    }
 }
 
 
@@ -219,7 +238,7 @@ void BitSet::Print(ostream& output_stream){
 
 
 //return (bit-by-bit) true if *this == m, and false otherwise. This method requires *this and m to have the same size
-inline Bits BitSet::operator == (BitSet& m){
+Bits BitSet::operator == (BitSet& m){
     
     unsigned int p;
     Bits result;
@@ -246,7 +265,7 @@ inline Bits BitSet::operator == (BitSet& m){
 }
 
 //Confronto *this con m e scrivo in result il risultato. result è uguale a 1 se *this < m e a 0 altrimenti
-inline Bits BitSet::operator < (const BitSet& m){
+Bits BitSet::operator < (const BitSet& m){
     
     int s;
     Bits result, check;
@@ -296,12 +315,11 @@ unsigned long long int BitSet::Get(unsigned int p){
     }
     
     return result;
-    
 }
 
 
 //overload of [] operator. IT IS IMPORTANT THAT THIS RETURNS A REFERENCE, NOT AN UnsignedInt: OTHERWISE THE RETURNED OBJECT, WHEN MODIFIED, WILL NOT CHANGE *this
-inline Bits& BitSet::operator [] (const unsigned int& i){
+Bits& BitSet::operator [] (const unsigned int& i){
     
     return((b[i]));
     
@@ -310,7 +328,7 @@ inline Bits& BitSet::operator [] (const unsigned int& i){
 
 
 //return *this + *addend
-inline BitSet BitSet::operator + (BitSet* addend) {
+BitSet BitSet::operator + (BitSet* addend) {
     
     BitSet a;
     
@@ -323,7 +341,7 @@ inline BitSet BitSet::operator + (BitSet* addend) {
 
 
 //return *this - m
-inline BitSet BitSet::operator - (BitSet* addend) {
+BitSet BitSet::operator - (BitSet* addend) {
     
     BitSet t;
     
@@ -336,7 +354,7 @@ inline BitSet BitSet::operator - (BitSet* addend) {
 
 
 //return *this + *addend and write the carry in *carry
-inline BitSet BitSet::Add(BitSet* addend, Bits* carry) {
+BitSet BitSet::Add(BitSet* addend, Bits* carry) {
     
     BitSet a;
     
@@ -350,7 +368,7 @@ inline BitSet BitSet::Add(BitSet* addend, Bits* carry) {
 
 
 //return *this + *subrahend and write the borrow in *borrow
-inline BitSet BitSet::Substract(BitSet* subtrahend, Bits* borrow) {
+BitSet BitSet::Substract(BitSet* subtrahend, Bits* borrow) {
     
     BitSet t;
     
@@ -363,7 +381,7 @@ inline BitSet BitSet::Substract(BitSet* subtrahend, Bits* borrow) {
 
 
 //add addend to *this,  and store the result in *this. This method requires this->GetSize() to be >= addend.GetSize()
-inline void BitSet::operator += (BitSet* addend){
+void BitSet::operator += (BitSet* addend){
     
     Bits carry, t;
     
@@ -408,7 +426,7 @@ void BitSet::AddTo(BitSet* addend, Bits* carry){
 }
 
 //remove useless bits on the tail of *this that contain all 0s
-inline void BitSet::Normalize(void){
+void BitSet::Normalize(void){
     
     int p;
     
@@ -425,7 +443,7 @@ inline void BitSet::Normalize(void){
 }
 
 //remove useless bits on the tail of *this that contain all 0s, as long as this deletion does not make the size of *this < than n
-inline void BitSet::Normalize(unsigned int n){
+void BitSet::Normalize(unsigned int n){
     
     int p;
     
@@ -471,7 +489,7 @@ void BitSet::AddTo(Bits* addend, Bits* carry){
 
 
 //substract m to *this and write the result in *this
-inline void BitSet::operator -= (BitSet* subtrahend) {
+void BitSet::operator -= (BitSet* subtrahend) {
     
     
     BitSet subtrahend_t;
@@ -578,7 +596,7 @@ void BitSet::SubstractTo(Bits* subtrahend, Bits* borrow) {
 
 
 //write the one-complement of *this with respect to a size 'size' of the binary representation and write it into *this
-inline void BitSet::ComplementTo(unsigned int size){
+void BitSet::ComplementTo(unsigned int size){
     
     unsigned int s;
     
@@ -599,7 +617,7 @@ inline void BitSet::ComplementTo(unsigned int size){
 
 
 //write the one-complement of *this  and write it into *this
-inline void BitSet::ComplementTo(void){
+void BitSet::ComplementTo(void){
     
     for(unsigned int s=0; s<GetSize(); s++){
         (b[s]).ComplementTo();
@@ -610,7 +628,7 @@ inline void BitSet::ComplementTo(void){
 
 
 //set to zero the first bits of *this that is equal to one (starting from the last bit) and leave the others unchanged, write the result in *this
-inline void BitSet::RemoveFirstSignificantBit(void){
+void BitSet::RemoveFirstSignificantBit(void){
     
     int s;
     Bits check_old, check_new;
@@ -705,7 +723,7 @@ void BitSet::operator &= (Bits* m){
 
 
 //perform (bit-by-bit) an & between  b[s] and *m ,and write the result in b[s] for all s = start, ..., end-1
-inline void BitSet::AndTo(Bits* m, unsigned int start, unsigned int end){
+void BitSet::AndTo(Bits* m, unsigned int start, unsigned int end){
     
     for(unsigned int s=start; s<end; s++){
         
