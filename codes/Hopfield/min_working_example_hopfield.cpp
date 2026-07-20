68bab47a4349243d6375eb1bc8e9f203a1399fa2
dumontbast <dumontbast@gmail.com>
2026-07-20 13:25:26 +0200

    .

--- codes/Hopfield/min_working_example_hopfield.cpp
+++ codes/Hopfield/min_working_example_hopfield.cpp
@@ -172,7 +172,7 @@ int main() {
 
                 gsl_rng_set(ran_evolve, 42);
                 clock_gettime(CLOCK_MONOTONIC, &t0);
-                bits.runSweeps_overlaps_DEBUG(ran_evolve, false, 0, 0);
+                bits.runSweeps(ran_evolve, false, 0, 0);
                 clock_gettime(CLOCK_MONOTONIC, &t1);
                 bits.toCanonical();
 
